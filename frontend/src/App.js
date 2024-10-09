cb4b46988df43edc71e19c9cbc3a037c8a4fbbba
aditiiprasad <aditi03prasad@gmail.com>
2024-10-09 22:27:08 +0530

    homepage chatpage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
 
-import { Button } from '@chakra-ui/react';
+
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Homepage from './pages/Homepage';
+import ChatPage from './pages/ChatPage';
 
 function App() {
   return (
     <div className="App">
-      <Button colorScheme='blue'>Button</Button>
+      
+      <Routes> 
+          <Route path="/" element={<Homepage />} exact/> 
+          <Route path='/chats' element={<ChatPage />} />
+        </Routes>
+
     </div>
   );
 }
---
+++ frontend/src/pages/ChatPage.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+
+const ChatPage = () => {
+    const [chats,setChats]=useState([]);
+
+ const fetchChats = async () => {
+      const response = await axios.get('/api/chat')
+
+      setChats(response.data); 
+ };
+
+ useEffect(()=> {
+   fetchChats();
+ },[])
+
+  return (
+    <div>
+      {chats.map( (chat) =>  (
+        <div> {chat.chatName}</div>
+    ))}
+    </div>
+  )
+}
+
+export default ChatPage
