b12ea7111246687713c9d7a3cd26656d754c5266
aditiiprasad <aditi03prasad@gmail.com>
2024-10-10 10:01:12 +0530

    login signup UI complete

--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -1,10 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Stack, HStack, VStack, FormControl, FormLabel, Input, InputGroup, InputRightElement, Button } from '@chakra-ui/react'
 
 const Login = () => {
+const [show, setShow] = useState(false);
+const [email, setEmail] = useState();
+const [password, setPassword] = useState();
+
+
+const handleClick = () => setShow(!show);
+
+const submitHandler = () => {
+
+}
+
   return (
-    <div>
-      login
-    </div>
+    <VStack spacing='5px'>
+         
+         <FormControl id='email' isRequired>
+          <FormLabel>Email</FormLabel>
+            <Input
+            borderColor={"red.600"}
+            placeholder='Enter your Email'
+            onChange={(e) =>setEmail(e.target.value) }
+            />
+         </FormControl>
+
+         <FormControl id='password' isRequired>
+          <FormLabel>Password</FormLabel>
+            <InputGroup>
+             <Input
+              borderColor={"red.600"}
+              type={show ? "text": "password"}
+              placeholder='Enter your Password'
+              onChange={(e) =>setPassword(e.target.value) }
+             />
+             <InputRightElement width="4.5rem">
+              <Button color={"red.400"} h="1.75rem" size="sm" onClick={handleClick}>
+              {show? "Hide" : "Show"}
+              </Button>          
+             </InputRightElement>  
+            </InputGroup>    
+         </FormControl>
+
+        
+
+          <Button
+           colorScheme="red"
+           width="100%"
+           style={{ marginTop:15 }}
+           onClick={submitHandler}
+           >
+            Log In
+          </Button>
+
+          <Button 
+           
+           variant="solid"
+           colorScheme="gray"
+           width="100%"
+           onClick={() => {
+            setEmail("guest@example.com");
+            setPassword("123456");
+           }}
+          >
+            Get Guest User Credentials
+          </Button>
+
+    </VStack>
   )
 }
 
