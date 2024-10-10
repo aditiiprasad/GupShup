7ecff9b2d775dc604d408281af9f868a76985c2b
aditiiprasad <aditi03prasad@gmail.com>
2024-10-10 10:50:12 +0530

    mongoDB connected

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
  const express = require('express');
  const dotenv = require('dotenv');
- const {chats} = require("./data/data")
-
- const app = express();
+ const {chats} = require("./data/data");
+ const connectDB = require('./config/db');
+ const colors = require("colors");
+ 
+ 
  dotenv.config();
+ connectDB()
+ const app = express();
 
  app.get('/', (req,res) => {
     res.send("API IS RUNNING")
@@ -23,4 +27,4 @@
 
 const PORT = process.env.PORT || 5000
 
- app.listen(5000, console.log(`SErver started on port ${PORT}`))
+ app.listen(5000, console.log(`Server started on port ${PORT}`.yellow.bold))
