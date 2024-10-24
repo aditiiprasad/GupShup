9508b5ebbf342a8bafe803e5548e6acd3885b235
aditiiprasad <aditi03prasad@gmail.com>
2024-10-24 23:45:36 +0530

    Profile Modal

--- frontend/src/components/miscellaneous/ProfileModal.js
+++ frontend/src/components/miscellaneous/ProfileModal.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  useDisclosure,
+  IconButton,
+  Text,
+  Image,
+} from "@chakra-ui/react";
 
-const ProfileModal = () => {
+
+const ProfileModal = ({ user, children }) => {
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+
+
   return (
-    <div>
+    <>
       
-    </div>
+       
+
+
+    </>
   )
 }
 
