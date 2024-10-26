b145cb0d59d60e165c6694c88b4279f945e0639c
aditiiprasad <aditi03prasad@gmail.com>
2024-10-26 11:09:50 +0530

    search User Frontend

---
+++ frontend/src/components/ChatLoading.js
@@ -0,0 +1,24 @@
+import { Skeleton, Stack } from '@chakra-ui/react'
+import React from 'react'
+
+const ChatLoading = () => {
+  return (
+    <Stack>
+      <Skeleton height="45px" startColor="rgba(255, 0, 0, 0.2)" endColor="rgba(255, 0, 0, 0.4)"/>
+      <Skeleton height="45px" startColor="rgba(255, 0, 0, 0.2)" endColor="rgba(255, 0, 0, 0.4)"/>
+      <Skeleton height="45px" startColor="rgba(255, 0, 0, 0.2)" endColor="rgba(255, 0, 0, 0.4)"/>
+      <Skeleton height="45px" startColor="rgba(255, 0, 0, 0.2)" endColor="rgba(255, 0, 0, 0.4)"/>
+      <Skeleton height="45px" startColor="rgba(255, 0, 0, 0.2)" endColor="rgba(255, 0, 0, 0.4)"/>
+      <Skeleton height="45px" startColor="rgba(255, 0, 0, 0.2)" endColor="rgba(255, 0, 0, 0.4)"/>
+      <Skeleton height="45px" startColor="rgba(255, 0, 0, 0.2)" endColor="rgba(255, 0, 0, 0.4)"/>
+      <Skeleton height="45px" startColor="rgba(255, 0, 0, 0.2)" endColor="rgba(255, 0, 0, 0.4)"/>
+      <Skeleton height="45px" startColor="rgba(255, 0, 0, 0.2)" endColor="rgba(255, 0, 0, 0.4)"/>
+      <Skeleton height="45px" startColor="rgba(255, 0, 0, 0.2)" endColor="rgba(255, 0, 0, 0.4)"/>
+
+      
+      
+    </Stack>
+  )
+}
+
+export default ChatLoading
--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -1,13 +1,16 @@
 import React from 'react' ;
 import { useState } from "react";
 import { Box, Button, Text, Tooltip , Image, Menu, MenuButton, MenuList, Avatar, MenuItem, MenuDivider, Drawer ,DrawerBody,
-  DrawerContent, DrawerHeader, DrawerOverlay,} from '@chakra-ui/react';
+  DrawerContent, DrawerHeader, DrawerOverlay,Input} from '@chakra-ui/react';
 import logo from "../../images/logo.png";
 import { BellIcon, ChevronDownIcon } from "@chakra-ui/icons";
 import { ChatState } from '../../Context/ChatProvider';
 import ProfileModal from './ProfileModal';
 import { useNavigate } from 'react-router-dom';
 import { useDisclosure } from "@chakra-ui/hooks";
+import { useToast } from '@chakra-ui/react'
+import axios from 'axios';
+import ChatLoading from '../ChatLoading';
 
 
 
@@ -21,14 +24,56 @@ const SideDrawer = () => {
   const {user} = ChatState();
   const navigate = useNavigate();
   const { isOpen, onOpen, onClose } = useDisclosure()
+  const toast = useToast();
   
   const logoutHandler = () => {
     localStorage.removeItem("userInfo");
     navigate("/"); 
   };
 
+  const handleSearch =  async() => {
+
+    if (!search) {
+      toast({
+        title: "Please Enter something in search",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "top-left",
+      });
+      return;
+    }
+    try {
+      setLoading(true);
+
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+      const {data} = await axios.get('/api/user?search=${search}', config);
+
+      setLoading(false);
+      setSearchResult(data);
+
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: "Failed to Load the Search Results",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom-left",
+      });
+    }
+  };
+
+
+  const accessChat = (userId) => {
+
+  };
   return (
-    <>
+         <>
        
 <Box d="flex"
         justifyContent="space-between"
@@ -40,7 +85,7 @@ const SideDrawer = () => {
         background="rgba(201, 73, 73, 0.2)" 
         display="flex"
         >
-  <Tooltip  hasArrow placement="bottom-end">
+  <Tooltip label="Search User" hasArrow placement="bottom-end">
  
  <Button variant="ghost"
          colorScheme="red"
@@ -48,8 +93,10 @@ const SideDrawer = () => {
           bg: "red.500", 
           color: "white", 
           opacity: 0.8, 
+
         }}
         _focus={{ boxShadow: "none" }} 
+        onClick={onOpen}
          >
   
  <i className="fas fa-search"></i>
@@ -129,30 +176,44 @@ const SideDrawer = () => {
   </Box>
 </Box>
 
-<Drawer placement="left" onClose={onClose} isOpen={isOpen}>
-<DrawerOverlay />
-<DrawerContent>
 
-<DrawerHeader borderBottomWidth="1px">
-Search Users
-</DrawerHeader>
+  <Drawer placement='left' onClose={onClose} isOpen={isOpen}>
 
+<DrawerOverlay/>
+<DrawerContent bg="black" border="1px " borderRightColor="red" >
+  <DrawerHeader borderBottomWidth="1px" fontWeight="bold"  color="red.800" >Search User</DrawerHeader>
+  <DrawerBody>
+  <Box display="flex" pb={2}>
+  <Input
+                placeholder="Search by name or email"
+                mr={2}
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                bg="black"
+                color="white"
+                borderColor="red.500"
+                _placeholder={{ color: "gray.400" }}
+               
+                _focus={{ borderColor: "white" }}
+ 
+ 
+              />
+  <Button colorScheme="red"  onClick={handleSearch}>Go</Button>           
+
+  </Box>
+  {loading ? (<ChatLoading />  ): (searchResult?.map(user => (
+    <UserListItem 
+    key={user._id}
+                  user={user}
+                  handleFunction={() => accessChat(user._id)}
+    />
+  )) )}
+</DrawerBody> 
 </DrawerContent>
 
-<DrawerBody >
-
-<Box>
-
-
-</Box>
-
-</DrawerBody>
-
-</Drawer>
-
-
+  </Drawer> 
     </>
   )
 }
 
-export default SideDrawer
+export default SideDrawer ;
